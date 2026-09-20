3a2121a79be38380f33c05c7b135164d4aeb86a0
Tito-21 <t.sibo@alustudent.com>
2026-09-20 11:28:27 +0200

    encrypt password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Return a salted, hashed password as a byte string"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate that the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
