c6f0998f48fbc19d4ca01e444b0049afc6880770
Tito-21 <t.sibo@alustudent.com>
2026-09-20 11:02:32 +0200

    Fix filtered_logger.py file corruption

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -29,36 +29,3 @@ class RedactingFormatter(logging.Formatter):
         """Filter values in incoming log records using filter_datum"""
         message = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
-    cat > filtered_logger.py << 'EOF'
-#!/usr/bin/env python3
-"""Filtered logger module"""
-import logging
-import re
-from typing import List
-
-
-def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str
-) -> str:
-    """Obfuscate specified fields in a log message"""
-    pattern = r'(' + '|'.join(fields) + r')=[^' + separator + r']*'
-    return re.sub(pattern, lambda m: m.group(1) + '=' + redaction, message)
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Filter values in incoming log records using filter_datum"""
-        message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
-
